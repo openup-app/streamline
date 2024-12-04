702d7e0a0338153e55b10f0b1d8baddd1b240c67
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2024-12-03 23:10:49 -0600

    Emit h265 via ffmpeg pipe on iOS

--- flutter_app/lib/record.dart
+++ flutter_app/lib/record.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 import 'dart:typed_data';
+import 'dart:ui';
 
 import 'package:ffmpeg_kit_flutter_min/ffmpeg_kit.dart';
 import 'package:ffmpeg_kit_flutter_min/ffmpeg_kit_config.dart';
@@ -33,29 +34,28 @@ Future<Stream<List<int>>?> startRecording() async {
   final outputFile = File('${outputDir.path}/out.sdp');
   await outputFile.create();
 
-  // final pipe = await FFmpegKitConfig.registerNewFFmpegPipe();
-  // if (pipe == null) {
-  //   return null;
-  // }
+  final pipe = await FFmpegKitConfig.registerNewFFmpegPipe();
+  if (pipe == null) {
+    return null;
+  }
   final String command;
   if (Platform.isIOS) {
     command =
-        '''-hide_banner -s 1920x1080 -f avfoundation -framerate 60 -i \"0\" -c:v hevc_videotoolbox -b:v 25M -r 60 -g 2 -profile:v main10 -pix_fmt nv12 -t 10 -y -f rtp rtp://192.168.86.139:1234 -sdp_file ${outputFile.path}''';
+        '''-hide_banner -s 1920x1080 -f avfoundation -framerate 30 -i \"0\" -c:v hevc_videotoolbox -b:v 25M -r 30 -g 2 -profile:v main10 -pix_fmt nv12 -t 10 -y -f hevc $pipe''';
   } else {
     command =
         '''-hide_banner -s 1920x1080 -f android_camera -framerate 30 -i "0" -c:v hevc -b:v 25M -r 30 -g 2 -profile:v main10 -pix_fmt nv12 -t 10 -y -f rtp rtp://192.168.86.22:1234 -sdp_file ${outputFile.path}''';
   }
 
   // Read the output from the pipe
-  final completer = Completer<File>();
-  // final outputSink = outputFile.openWrite();
-  // final byteStream = File(pipe).openRead();
+  final outputSink = outputFile.openWrite();
+  final byteStream = File(pipe).openRead();
   // byteStream.listen(outputSink.add);
 
   await FFmpegKit.executeAsync(
     command,
     (_) async {
-      // await FFmpegKitConfig.closeFFmpegPipe(pipe);
+      await FFmpegKitConfig.closeFFmpegPipe(pipe);
       // await outputSink.flush();
       // await outputSink.close();
 
@@ -68,8 +68,8 @@ Future<Stream<List<int>>?> startRecording() async {
   final sdp = await outputFile.readAsString();
   print(sdp);
 
-  // return byteStream;
-  return Stream.fromIterable([]);
+  return byteStream;
+  // return Stream.fromIterable([]);
   // await FFmpegKit.execute(command);
   // return outputFile;
 }
